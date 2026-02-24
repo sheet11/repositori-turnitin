a67230410510ff3f7dd3a0e366f90e476ab8c7e8
sheet <ahmadreynaldo@gmail.com>
2026-02-24 14:40:12 +0700

    first commit

---
+++ database/migrations/2026_02_24_051342_create_dokumen_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    Schema::create('dokumen', function (Blueprint $table) {
+        $table->id();
+        $table->string('judul');
+        $table->enum('jenis_dokumen',['Skripsi','Jurnal','Proposal','KTI']);
+        $table->string('nim');
+        $table->string('file_asli');
+        $table->string('bukti_bayar')->nullable();
+        $table->timestamp('tanggal_upload');
+        $table->enum('status',['Pending','Di Proses','Ditolak','Selesai'])->default('Pending');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dokumen');
+    }
+};
