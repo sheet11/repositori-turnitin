9aa7c234d86c935eac2ab74cd27d5c7ce8d02414
sheet <ahmadreynaldo@gmail.com>
2026-08-14 13:03:50 +0700

    feat: add landing page layout for similarity monitoring system

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -350,11 +350,6 @@
                             <a href="{{ route('login') }}" class="btn btn-outline-primary btn-custom">
                                 Login
                             </a>
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="btn btn-primary btn-custom shadow-sm">
-                                    Daftar Akun
-                                </a>
-                            @endif
                         @endauth
                     @endif
                 </div>
